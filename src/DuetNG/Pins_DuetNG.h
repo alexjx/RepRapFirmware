62855c9ba2d817f12b5ffb0bf9b01cd52affffd7
Jia Xin <alexjx@gmail.com>
2019-06-08 10:25:11 +0800

    disable laser

--- src/DuetNG/Pins_DuetNG.h
+++ src/DuetNG/Pins_DuetNG.h
@@ -25,7 +25,7 @@ constexpr size_t NumFirmwareUpdateModules = 4;		// 3 modules, plus one for manua
 #define SUPPORT_ROLAND			0					// set nonzero to support Roland mill
 #define SUPPORT_SCANNER			0					// set zero to disable support for FreeLSS scanners
 #define SUPPORT_LASER			0					// support laser cutters and engravers using G1 S parameter
-#define SUPPORT_IOBITS			1					// set to support P parameter in G0/G1 commands
+#define SUPPORT_IOBITS			0					// set to support P parameter in G0/G1 commands
 #define SUPPORT_DHT_SENSOR		0					// set nonzero to support DHT temperature/humidity sensors
 #define SUPPORT_WORKPLACE_COORDINATES	0			// set nonzero to support G10 L2 and G53..59
 #define SUPPORT_12864_LCD		0					// set nonzero to support 12864 LCD and rotary encoder
