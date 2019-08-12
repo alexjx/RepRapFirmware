c359fe9fd856346a09f97e8e6e066a844e604680
Jia Xin <alexjx@gmail.com>
2019-08-12 17:41:09 +0800

    implementing the HDC1080 sensor

---
+++ src/Heating/Sensors/Hdc1080TemperatureSensor.h
@@ -0,0 +1,42 @@
+/*
+ * Hdc1080TemperatureSensor.h
+ *
+ *  Created on:
+ *      Author: J.Xin
+ */
+
+#ifndef SRC_HEATING_HDC1080TEMPERATURESENSOR_H_
+#define SRC_HEATING_HDC1080TEMPERATURESENSOR_H_
+
+#include "TemperatureSensor.h"
+
+// This class represents a DHT temperature sensor
+class Hdc1080TemperatureSensor : public TemperatureSensor
+{
+public:
+	Hdc1080TemperatureSensor(unsigned int channel);
+	~Hdc1080TemperatureSensor();
+
+	GCodeResult Configure(unsigned int mCode, unsigned int heater, GCodeBuffer& gb, const StringRef& reply) override;
+	void Init() override;
+
+protected:
+	TemperatureError TryGetTemperature(float& t) override;
+};
+
+// This class represents a DHT humidity sensor
+class Hdc1080HumiditySensor : public TemperatureSensor
+{
+public:
+	Hdc1080HumiditySensor(unsigned int channel);
+	~Hdc1080HumiditySensor();
+
+	GCodeResult Configure(unsigned int mCode, unsigned int heater, GCodeBuffer& gb, const StringRef& reply) override;
+	void Init() override;
+
+protected:
+	TemperatureError TryGetTemperature(float& t) override;
+};
+
+
+#endif /* SRC_HEATING_HDC1080TEMPERATURESENSOR_H_ */
